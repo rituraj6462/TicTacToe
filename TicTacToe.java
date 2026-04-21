9241dc38ab01c442d8bfc5b41088f7cb28375893
rituraj6462 <rr6462@srmist.edu.in>
2026-04-21 13:49:34 +0530

    UC5: Validate move within bounds and check if cell is empty

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,19 +1,28 @@
 public class TicTacToe {
 
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
+
     public static void main(String[] args) {
-        int slot = 7;
-
-        System.out.println("Row: " + getRowFromSlot(slot));
-        System.out.println("Column: " + getColFromSlot(slot));
+        System.out.println(isValidMove(1, 1));
     }
 
-    // Convert slot (1–9) → row (0–2)
-    static int getRowFromSlot(int slot) {
-        return (slot - 1) / 3;
-    }
+    // Validate move
+    static boolean isValidMove(int row, int col) {
 
-    // Convert slot (1–9) → column (0–2)
-    static int getColFromSlot(int slot) {
-        return (slot - 1) % 3;
+        // Check bounds
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
+        }
+
+        // Check if cell is empty
+        if (board[row][col] != '-') {
+            return false;
+        }
+
+        return true;
     }
 }
